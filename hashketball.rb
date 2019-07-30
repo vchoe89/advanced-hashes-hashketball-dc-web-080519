f92071c0bb519b84bf3613753a3ad961a4aec1d4
Victor Jian Choe <vchoe89@gmail.com>
2019-07-30 02:57:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,7 @@ def game_hash
     home: {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => {
+      :players => [
         "Alan Anderson" => {
           :number => 0,
           :shoe => 16,
